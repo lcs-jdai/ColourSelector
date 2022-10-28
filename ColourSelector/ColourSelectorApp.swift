05bbee1acbf9ca72b941ec957ba5bbba8afd8045
Jerry Dai <jdai@lcs.on.ca>
2022-10-28 14:36:22 -0400

    Initial Commit

---
+++ ColourSelector/ColourSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColourSelectorApp.swift
+//  ColourSelector
+//
+//  Created by Dai Jiaze on 2022-10-28.
+//
+
+import SwiftUI
+
+@main
+struct ColourSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
